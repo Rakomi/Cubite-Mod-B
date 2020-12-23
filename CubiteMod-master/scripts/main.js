23bf41ec774c9b62cf9c980f590a6302a8bce1c2
Rakomi <66920510+Rakomi@users.noreply.github.com>
2020-12-24 00:41:42 +0200

    Add files via upload

---
+++ CubiteMod-master/scripts/main.js
@@ -0,0 +1,34 @@
+//units
+require("units/testunit4");
+//blocks
+require("blocks/activationmatter-weaver");
+require("blocks/atmospheric-cooler");
+require("blocks/impact-projector");
+require("blocks/multi-projector");
+require("blocks/multi-cultivator");
+require("blocks/power-processor");
+require("blocks/coredvalut");
+require("blocks/boostwater");
+require("blocks/plastsalvo");
+require("blocks/titanium-launch-pad")
+
+//experiement
+require("experiment/page");
+require("experiment/oregen");
+
+require("campaign/planet");
+
+print("Load Complete!");
+
+
+
+
+
+
+/*
+credit(){
+  younggam: "answering tons of question",
+  deltaNedas: "refer to routorio, rtfm",
+  sk7725: "answering tons of question, refer to testers"
+}
+*/
